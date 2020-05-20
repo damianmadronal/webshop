a68950c5a9f8bd068bfcbc11de4a2fa20825166a
Damian Madronal <99051248@mydavinci.nl>
2020-05-20 15:50:46 +0200

    changed route

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('category_overview', 'CategoryController@index');
 Route::get('category/{id}', 'ProductController@ProductOverview');
 Route::get('product/{id}', 'ProductController@index');
