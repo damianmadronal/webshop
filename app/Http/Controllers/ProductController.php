e54a9c51f447cad269a1a865e6203018716424c8
Damian Madronal <99051248@mydavinci.nl>
2020-05-27 14:37:23 +0200

    added comments to productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,6 +11,12 @@ use Session;
 
 class ProductController extends Controller
 {
+    /**
+     * Gives back products and category name to view
+     *
+     * @param int $id
+     * @return void
+     */
     public function productOverview($id)
     {
         $categoryName = DB::table('category')->get()->where('id', $id)->pluck('name');
@@ -19,6 +25,12 @@ class ProductController extends Controller
         return view('product_overview', ['products' => $products, 'categoryName' => $categoryName]);
     }
 
+    /**
+     * Gives back products to view
+     *
+     * @param int $id
+     * @return void
+     */
     public function productIndex($id)
     {
         $product = DB::table('product')->get()->where('id', $id)[$id - 1];
@@ -26,12 +38,20 @@ class ProductController extends Controller
         return view('product', ['product' => $product]);
     }
 
+    /**
+     * cool
+     *
+     * @param Request $request
+     * @param int $id
+     * @return void
+     */
     public function add(Request $request, $id)
     {
         $oldCart = Session::has('cart') ? Session::get('cart') : null;
         $cart = new Cart($oldCart);
         $cart->add($request, Product::find($id));
-        // dd($request->session()->get('cart'));
+        // $request->session()->flush();
+        dd($request->session()->get('cart'));
 
         return back()->with('success', 'Succesfully added item to cart!');
     }
