8a5e2db5a34feb4a5d39ba5c7b72315f1c01facc
Damian Madronal <99051248@mydavinci.nl>
2020-07-01 15:30:06 +0200

    made shopping cart view

--- resources/views/shopping-cart.blade.php
+++ resources/views/shopping-cart.blade.php
@@ -2,10 +2,15 @@
 
 @section('content')
 <div class="container">
+ @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="row">
         <div class="col">
-        @if(Session::has('cart'))
-            @foreach($products as $product)
+        @if($cart != null)
+            @foreach($cart->items as $product)
                 <div class="cart-item-container">
                     <div class="row py-5">
                         <div class="col">
@@ -15,10 +20,17 @@
                             <h2>{{$product['item']->name}}</h2>
                         </div>
                         <div class="col">
-                            <p>{{$product['quantity']}}</p>
+                            <form action="{{ route('shoppingCart.update', $product["item"]->id) }}" method="POST">
+                            @csrf
+                            <input id="quantityInput" class="quantityInput" name="quantityInput" type="number" size="20" value="{{ $product["quantity"] }}">
+                            <input class="btn btn-primary" type="submit" value="Apply" >
+                        </form>
                         </div>
                         <div class="col">
-                            <p><strong>&euro;{{$product['price']}}</strong></p>
+                            <p><strong>&euro;{{$product['price']}},-</strong></p>
+                        </div>
+                        <div class="col">
+                        <p class="delete-button"><a href="{{ route('product.delete', ['id' => $product['item']->id]) }}"><i class="fab fa-xbox"></i></a></p>
                         </div>
                     </div>
                 </div>
@@ -27,13 +39,13 @@
             <div class="order-container">
                 <div class="row order-details">
                     <div class="col">
-                        <p>Total products ({{$totalQuantity}}) - <strong>&euro;{{$totalPrice}}</strong></p>
-                        <button type="button" class="btn btn-danger">Order</button>
+                        <p>Total products ({{$cart->getTotalQuantity()}}) - <strong>&euro;{{$cart->getTotalPrice()}}</strong></p>
+                        <button type="button" class="btn btn-danger"><a href="{{ route('product.order') }}">Order</a></button>
                     </div>
                 </div>
             </div>
-
-
+    @else
+    <p><small>No items found</small></p>
         @endif
         </div>
     </div>
