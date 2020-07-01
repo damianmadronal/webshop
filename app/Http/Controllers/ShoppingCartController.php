ceeed4752e95250cf1340c7e94e73155429fa04d
Damian Madronal <99051248@mydavinci.nl>
2020-07-01 15:28:41 +0200

    update quantity and delete functions for shopping cart

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Session;
 use App\Cart\Cart;
+use DB;
+
 
 use Illuminate\Http\Request;
 
@@ -19,7 +21,6 @@ class ShoppingCartController extends Controller
         $this->middleware('auth');
     }
 
-
     /**
      * Give back cart items to view
      *
@@ -27,15 +28,81 @@ class ShoppingCartController extends Controller
      */
     public function getCart()
     {
-        if (!Session::has('cart')) {
-            return view('shopping-cart')->with(['products' => null]);
-        }
-        $oldCart = Session::get('cart');
-        $cart = new Cart($oldCart);
+        $cart = Session::has('cart') ? Session::get('cart') : null;
+        // $cart = new Cart($oldCart);
         return view('shopping-cart')->with([
-            'products' => $cart->items,
-            'totalPrice' => $cart->price,
-            'totalQuantity' => $cart->quantity
+            'cart' => $cart
+        ]);
+    }
+
+    public function updateQuantity(Request $request, $id)
+    {
+        $newQuantity = $request->input('quantityInput');
+
+        $oldCart = Session::get('cart');
+
+        $oldCart->quantity -= $oldCart->items[$id]['quantity'];
+        $oldCart->quantity += $newQuantity;
+
+        $oldCart->price -= $oldCart->items[$id]['price'];
+        $oldCart->price += $newQuantity * $oldCart->items[$id]['price'];
+
+        if ($oldCart->quantity == 0) {
+            $oldCart->items[$id]['price'] = $newQuantity * $oldCart->items[$id]['price'];
+        } else {
+            $oldCart->items[$id]['price'] = $newQuantity * ($oldCart->items[$id]['price']  / $oldCart->items[$id]['quantity']);
+        }
+
+        $oldCart->items[$id]['quantity'] = $newQuantity;
+        // $oldCart->items[$id]['price'] = $newQuantity * $oldCart->items[$id]['price'];
+
+        // $cart = new Cart($oldCart);
+
+        if ($oldCart->items[$id]['quantity'] < 1) {
+            return redirect()->route("product.delete", [$id]);
+        }
+
+        return view('shopping-cart')->with([
+            'cart' => $oldCart
+        ]);
+    }
+
+    public function deleteItem($id)
+    {
+        $oldCart = Session::get('cart');
+
+        unset($oldCart->items[$id]);
+
+
+        if (empty($oldCart->items)) {
+            Session::forget("cart");
+        }
+
+        $cart = Session::has('cart') ? Session::get('cart') : null;
+
+        return view('shopping-cart')->with([
+            'cart' => $cart
+        ]);
+    }
+
+    public function order()
+    {
+        $cart = Session::get('cart');
+
+        $content = [
+            "cart" => serialize($cart),
+            "user_id" => 1
+        ];
+
+        DB::table("orders")->insert($content);
+
+        Session::forget('cart');
+
+        $cart = Session::has('cart') ? Session::get('cart') : null;
+
+        return view('shopping-cart')->with([
+            'cart' => $cart,
+            'success', 'Succesfully placed order!'
         ]);
     }
 }
