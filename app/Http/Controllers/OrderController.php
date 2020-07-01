7432d00467adaaa545331df3c3e88ae6d49fea03
Damian Madronal <99051248@mydavinci.nl>
2020-07-01 15:28:09 +0200

    added ordercontroller and index function

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Auth;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $userId = Auth::user()->id;
+
+        $orders = DB::table('orders')->where('user_id', $userId)->get();
+
+        return view('orders', ['orders' => $orders]);
+    }
+}
